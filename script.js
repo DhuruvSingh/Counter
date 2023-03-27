3d2c0bb85b249f15fc93ad54aa36bfe927ffa6e5
DhuruvSingh <Dhuruvs096@gmail.com>
2023-03-27 12:24:48 +0530

    Added Comments for More Readibility

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const countValue = document.querySelector("#counter");
 
 
 const increment = () => {
+    // get the value from UI
     let value = parseInt(countValue.innerText);
+
+    // update the value
     value = value + 1;
+
     countValue.innerText = value;
 };
 
 const decrement = () => {
+    // get the value from UI
     let value = parseInt(countValue.innerText);
+
+    // update the value
     value = value - 1;
+
+    // set the value into UI
     countValue.innerText = value;
 
 };
